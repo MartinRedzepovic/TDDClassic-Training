1110dfc5575af57450e94a71647b301764c0b8ba
Martin Redzepovic <martin.redzepovic@siemens-healthineers.com>
2021-09-06 12:57:47 +0200

    Initial commit

---
+++ TDDTraining-Classic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDTraining_Classic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
