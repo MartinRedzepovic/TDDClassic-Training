20c68d185e48c51c4800155e99995063cd1a7efd
Martin Redzepovic <martin.redzepovic@siemens-healthineers.com>
2021-09-06 16:01:50 +0200

    Implement Fraction.Add to support adding fraction with the same denominator

---
+++ TDDTraining-Classic.Tests/FractionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TDDTraining_Classic.Tests
+{
+    [TestFixture]
+    class FractionTests
+    {
+        [Test]
+        public void Add_TwoFractionsWithSameDenominator()
+        {
+            // given
+            var fraction1 = new Fraction(1, 7);
+            var fraction2 = new Fraction(5, 7);
+
+            // when
+            var result = fraction1.Add(fraction2);
+
+            // then
+            result.ToString().Should().Be("6/7");
+        }
+    }
+}
---
+++ TDDTraining-Classic/Fraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTraining_Classic
+{
+    public class Fraction
+    {
+        public int Nominator { get;  }
+        public int Denominator { get; }
+
+        public Fraction(int nominator, int denominator)
+        {
+            Nominator = nominator;
+            Denominator = denominator;
+        }
+
+        public Fraction Add(Fraction other)
+        {
+            return new Fraction(Nominator + other.Nominator, Denominator);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nominator}/{Denominator}";
+        }
+    }
+}
